78fdf8c77b781604683a5e5ae3c466f3408b7c27
MyrtoTs <42088027+MyrtoTs@users.noreply.github.com>
2018-09-06 20:09:03 +0300

    Update fetch_data.py
    database with column "length" (number of words in comment)

--- fetch_data.py
+++ fetch_data.py
@@ -1,47 +1,50 @@
-import subprocess
-from subprocess import call
+# import subprocess
+# from subprocess import call
 import json
 import sqlite3
 
 
-def download_file(url, path):
-    print(subprocess.check_output(['wget', '-P', path, url]).decode())
+# def download_file(url, path):
+#     print(subprocess.check_output(['wget', '-P', path, url]).decode())
 
 
-def extract_file(full_path):
-    extension = full_path.split('.')[-1]
+# def extract_file(full_path):
+#     extension = full_path.split('.')[-1]
 
-    if extension == 'bz2':
-        print(subprocess.check_output(['bzip2', '-dk', full_path]).decode())
-    elif extension == 'xz':
-        print(subprocess.check_output(['unxz', full_path]).decode())
-
-
-dataset_path = '/data/data1/users/amandalios/reddit_dataset/'
-combined_file_path = dataset_path + 'combined_file'
-database_path = dataset_path + 'reddit_sqlite_database.db'
+#     if extension == 'bz2':
+#         print(subprocess.check_output(['bzip2', '-dk', full_path]).decode())
+#     elif extension == 'xz':
+#         print(subprocess.check_output(['unxz', full_path]).decode())
 
 
+# dataset_path = '/data/data1/users/amandalios/reddit_dataset/'
+# combined_file_path = dataset_path + 'combined_file'
+# database_path = dataset_path + 'reddit_sqlite_database.db'
 
-urls_list = [
-    'http://files.pushshift.io/reddit/comments/RC_2017-01.bz2',
-    'http://files.pushshift.io/reddit/comments/RC_2017-02.bz2'
-]
 
-file_list = [dataset_path + url.split('/')[-1] for url in urls_list]
 
-print('\n\n     DOWNLOAD AND EXTRACT FILES      \n\n')
+# urls_list = [
+#     'http://files.pushshift.io/reddit/comments/RC_2017-01.bz2',
+#     'http://files.pushshift.io/reddit/comments/RC_2017-02.bz2'
+# ]
 
-for url, file in zip(urls_list, file_list):
-    print('Downloading from url {}...'.format(url))
-    download_file(url, dataset_path)
+# file_list = [dataset_path + url.split('/')[-1] for url in urls_list]
 
-    print('Extracting file {}...'.format(file))
-    extract_file(file)
+# print('\n\n     DOWNLOAD AND EXTRACT FILES      \n\n')
 
-print('\n\n     COMBINE FILES INTO ONE      \n\n')
-command = ' '.join(['cat'] + [dataset_path + f.split('/')[-1].split('.')[0] for f in file_list] + ['>', combined_file_path])
-call(command, shell=True)
+# for url, file in zip(urls_list, file_list):
+#     print('Downloading from url {}...'.format(url))
+#     download_file(url, dataset_path)
+
+#     print('Extracting file {}...'.format(file))
+#     extract_file(file)
+
+# print('\n\n     COMBINE FILES INTO ONE      \n\n')
+# command = ' '.join(['cat'] + [dataset_path + f.split('/')[-1].split('.')[0] for f in file_list] + ['>', combined_file_path])
+# call(command, shell=True)
+
+database_path = '/home/myrto/pink_dir/github/december_17_db.db'
+combined_file_path = '/home/myrto/pink_dir/RC_2017-12'
 
 print('\n\n     CREATING SQLITE DATABASE      \n\n')
 
@@ -54,18 +57,31 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS parents(id TEXT PRIMARY KEY,
                                                           post_body TEXT,
                                                           subreddit TEXT,
                                                           created_utc INT,
-                                                          score INT)""")
+                                                          score INT,
+                                                          length INT)""")
 cursor.execute("""CREATE TABLE IF NOT EXISTS replies(id TEXT PRIMARY KEY,
                                                           post_body TEXT,
                                                           subreddit TEXT,
                                                           created_utc INT,
-                                                          score INT)""")
+                                                          score INT,
+                                                          length INT)""")
 
 # go through the data
 
 row_counter = 0  # counter for the rows
 paired_rows = 0  # counter for rows that are paired, as in question-answer
 
+
+def acceptable_comment(body):
+    """basic test to see if comment is not too short or too long"""
+    if (len(body.split(' ')) > 100) or (len(body) < 1):
+        return False
+    elif len(body) > 3000:
+        return False
+    elif (body == '[deleted]' or body == '[removed]'):
+        return False
+    else:
+        return True
 
 def sanitize_body(body):
     """sanitize body of reddit posts"""
@@ -86,15 +102,15 @@ def transaction_bldr(sql):
         sql_t = []
 
 
-def sql_insert_parent(id, reply_id, post_body, subreddit, created_utc, score):
-    sql = """INSERT INTO parents (id, reply_id, post_body, subreddit, created_utc, score) VALUES ("{}","{}","{}","{}",{},{});""".format(
-        id, reply_id, post_body, subreddit, created_utc, score)
+def sql_insert_parent(id, reply_id, post_body, subreddit, created_utc, score,length):
+    sql = """INSERT INTO parents (id, reply_id, post_body, subreddit, created_utc, score, length) VALUES ("{}","{}","{}","{}",{},{},{});""".format(
+        id, reply_id, post_body, subreddit, created_utc, score,length)
     transaction_bldr(sql)
 
 
-def sql_insert_reply(id, post_body, subreddit, created_utc, score):
-    sql = """INSERT INTO replies (id, post_body, subreddit, created_utc, score) VALUES ("{}","{}","{}",{},{});""".format(
-        id, post_body, subreddit, created_utc, score)
+def sql_insert_reply(id, post_body, subreddit, created_utc, score,length):
+    sql = """INSERT INTO replies (id, post_body, subreddit, created_utc, score,length) VALUES ("{}","{}","{}",{},{},{});""".format(
+        id, post_body, subreddit, created_utc, score,length)
     transaction_bldr(sql)
 
 
@@ -111,13 +127,16 @@ with open(combined_file_path, buffering=10000) as f:
         parent_id = row['parent_id'].split('_')[1]
         comment_body = sanitize_body(row['body'])
         score = row['score']
+        
+        if score >= 2:
+            if acceptable_comment(comment_body):
 
-        if parent_id not in post_pairs:
-            post_pairs[parent_id] = (id, score)
-        else:
-            other_id, other_score = post_pairs[parent_id]
-            if score > other_score:
-                post_pairs[parent_id] = (id, score)
+                if parent_id not in post_pairs:
+                    post_pairs[parent_id] = (id, score)
+                else:
+                    other_id, other_score = post_pairs[parent_id]
+                    if score > other_score:
+                        post_pairs[parent_id] = (id, score)
 
 print('\n\nWe have {} post pairs\n\n'.format(len(post_pairs)))
 
@@ -139,14 +158,15 @@ with open(combined_file_path, buffering=10000) as f:
         subreddit = row['subreddit']
         created_utc = row['created_utc']
         score = row['score']
-
+        length = len(post_body.split(' '))
+        
         if id in parent_ids:
             # it's a parent
             sql_insert_parent(id, post_pairs[id][0], post_body, subreddit,
-                              created_utc, score)
+                              created_utc, score, length)
         if id in reply_ids:
             # it's a reply
-            sql_insert_reply(id, post_body, subreddit, created_utc, score)
+            sql_insert_reply(id, post_body, subreddit, created_utc, score,length)
 
 
 cursor.close()
