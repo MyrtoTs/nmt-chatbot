c8d6be33308246744b82cf0c95a7f9cdeaa11dd0
MyrtoTs <42088027+MyrtoTs@users.noreply.github.com>
2018-09-18 14:08:51 +0300

    Update LDA_pseudodoc.py
    To avoid urls.

--- LDA_pseudodoc.py
+++ LDA_pseudodoc.py
@@ -1,3 +1,5 @@
+""" creates LDA_files from the same subreddit with size between 2.000 and 2.200 words"""
+
 import sqlite3
 import random
 
@@ -13,32 +15,23 @@ ps = cursor.fetchall()
 cursor.execute("""SELECT * FROM parents""")
 ps2 = cursor.fetchall()
 
-with open(LDA_corpus_path +'LDA_pseudodocuments.txt' ,'w') as f:
+with open(LDA_corpus_path +'LDA_pseudodocuments_clear.txt' ,'w') as f:
     for k in ps:
         count_words = 0
-        doc = 0
         text = ""
         for parent_id, reply_id, parent_body, parent_subreddit, parent_utc_time, parent_score, parent_length in ps2:
             if parent_subreddit == (str(k)[2:-3]) :
                 cursor.execute("""SELECT * FROM replies WHERE id='{}'""".format(reply_id))
                 reply = cursor.fetchone()
                 if reply:
-                    reply_id, reply_body, reply_subreddit, reply_utc_time, reply_score, reply_length = reply
-                    count_words = count_words + parent_length + reply_length
-                    text = text + parent_body + reply_body
-                if(count_words > 2000):
-                    f.write(str(text) + '\n')
-                    doc += 1
-                    text = ""
-                    count_words = 0
+                    reply_id, reply_body, reply_subreddit, reply_utc_time, reply_score, reply_length = reply             
+                    to_add = parent_body + reply_body
+                    if not('http' in to_add):
+                        count_words = count_words + parent_length + reply_length
+                        text = text + to_add
+            if count_words >= 2000:
+                f.write(str(text) + '\n')                
+                break
 f.close()
-
-f = open(LDA_corpus_path + 'LDA_pseudodocuments.txt', 'r')
-lines = file.readlines()
-random.shuffle(lines)
-f.close()
-
-f_10000 = open(LDA_corpus_path + 'LDA_pseudodocuments_10000.txt', 'w')
-f_10000.write(lines[0:10000])
-
-f_10000.close()
+                
+          
